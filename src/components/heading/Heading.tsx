b3acd72eec2c7df6c5385e62134803be29b8ff9b
MacJ13 <jacdim02@gmail.com>
2024-11-21 19:32:59 +0100

    Update Heading component
    Return h3 element when condition statement is true

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -1,8 +1,8 @@
 import { HeadingProps } from "../../utils/types/components";
 
 const Heading = ({ type = "h1", className, children }: HeadingProps) => {
-  if (type === "h1") return <h1 className={className}>{children}</h1>;
-  else if (type === "h2") return <h2 className={className}>{children}</h2>;
+  if (type === "h2") return <h2 className={className}>{children}</h2>;
+  else if (type === "h3") return <h3 className={className}>{children}</h3>;
 
   return <h1 className={className}>{children}</h1>;
 };
