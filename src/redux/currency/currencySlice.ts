8017ae8b253de8a44e0e6351c22e1f4a32a240d8
MacJ13 <jacdim02@gmail.com>
2024-11-21 20:56:08 +0100

    Update CurrencyState type in currencySlice
    Create NBP type
    
    Create and add initialBaseNBP and initialCurrentNBp objects
    into initial State

--- src/redux/currency/currencySlice.ts
+++ src/redux/currency/currencySlice.ts
@@ -5,6 +5,7 @@ import {
   CurrencyState,
   CurrencyOrigin,
   CurrencyCounter,
+  NBP,
 } from "../types/currency";
 
 // Define the TS type for the counter slice's state
@@ -18,9 +19,31 @@ const initialCounter = {
   resultCurrency: 0,
 };
 
+const initialBaseNBP: NBP = {
+  type: "nbpBase",
+  label: "Waluta bazowa",
+  table: "a",
+  currency: "",
+  date: "",
+  value: 0,
+};
+
+const initialCurrentNBP: NBP = {
+  type: "nbpCurrent",
+  label: "Waluta kwotowana",
+  table: "b",
+  currency: "",
+  date: "",
+  value: 0,
+};
+
 const initialState: CurrencyState = {
   origin: "invoice",
   counter: initialCounter,
+  nbpTable: {
+    nbpBase: initialBaseNBP,
+    nbpCurrent: initialCurrentNBP,
+  },
 };
 
 // Slices contain Redux reducer logic for updating state, and
@@ -54,5 +77,6 @@ export const selectCurrencyOrigin = (state: RootState) => state.currency.origin;
 
 export const selectCurrencyCounter = (state: RootState) =>
   state.currency.counter;
+
 // export the slice reducer for use in the store configuration
 export default currencySlice.reducer;
--- src/redux/types/currency.ts
+++ src/redux/types/currency.ts
@@ -1,4 +1,4 @@
-type CurrencyCounter = {
+export type CurrencyCounter = {
   baseCurrency: string;
   counterCurrency: string;
   resultCurrency: number;
@@ -6,7 +6,21 @@ type CurrencyCounter = {
 
 export type CurrencyOrigin = "invoice" | "nbp";
 
+export type NBP = {
+  type: string;
+  label: string;
+  table: "a" | "b";
+  currency: string;
+  date: string;
+  value: number;
+};
+
 export type CurrencyState = {
   origin: CurrencyOrigin;
   counter: CurrencyCounter;
+  nbpTable: {
+    [key: string]: NBP;
+    nbpBase: NBP;
+    nbpCurrent: NBP;
+  };
 };
