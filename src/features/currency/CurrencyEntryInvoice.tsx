d75e9b07f52ba0765a6669a3ef222a1bd72579e0
MacJ13 <jacdim02@gmail.com>
2024-11-23 18:22:23 +0100

    Create CurrencyEntryInvoice component
    Set baseCurrency, counterCurrency and rate vallues
    for Invoice Currency data

---
+++ src/features/currency/CurrencyEntryInvoice.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import Flex from "../../components/flex/Flex";
+import Heading from "../../components/heading/Heading";
+import Input from "../../components/input/Input";
+import Label from "../../components/label/Label";
+import { useDispatch } from "react-redux";
+import { countCurrencyFromInvoice } from "../../redux/currency/currencySlice";
+import useFractionInput from "../../hooks/useFractionInput";
+import Button from "../../components/button/Button";
+
+const CurrencyEntryInvoice = () => {
+  const { inputValue, numberValue, handleNumberChange } = useFractionInput();
+
+  const [baseCurrency, setBaseCurrency] = useState<string>("");
+
+  const [counterCurrency, setCounterCurrency] = useState<string>("");
+
+  const dispatch = useDispatch();
+
+  return (
+    <Flex className="flex gap-5 flex-col items-center justify-center">
+      <Heading type="h3" className="text-xl text-center mb-2">
+        1a. Wypełnij dane (faktura)
+      </Heading>
+      <Flex>
+        <Flex className="flex gap-2.5 mb-2 items-center">
+          <Label className="font-semibold w-36" htmlFor="baseCurrency">
+            waluta bazowa:
+          </Label>
+          <Input
+            className="font-semibold bg-blue-100 border border-gray-300 text-gray-
+           900 text-sm rounded-md focus:outline-none focus:border-blue-400 
+           block w-14 p-1"
+            onChange={(e) => {
+              setBaseCurrency(e.target.value.toUpperCase());
+            }}
+            input={{
+              type: "text",
+              name: "baseCurrency",
+              id: "baseCurrency",
+              value: baseCurrency,
+            }}
+          />
+        </Flex>
+        <Flex className="flex gap-7 justify-center mb-8">
+          <Flex className="flex gap-2.5 items-center">
+            <Label className="font-semibold w-36" htmlFor="amountCurrency">
+              waluta kwotowana:
+            </Label>
+            <Input
+              className="font-semibold bg-blue-100 border border-gray-300 text-gray-
+          900 text-sm rounded-md focus:outline-none focus:border-blue-400 
+          block w-14 p-1"
+              onChange={(e) => {
+                setCounterCurrency(e.target.value.toUpperCase());
+              }}
+              input={{
+                type: "text",
+                name: "counterCurrency",
+                id: "counterCurrency",
+                value: counterCurrency,
+              }}
+            />
+          </Flex>
+          <Flex className="flex gap-2.5 items-center">
+            <Label htmlFor="amountCurrency">wartość:</Label>
+            <Input
+              className="font-semibold bg-blue-100 border border-gray-300 text-gray-
+            900 text-sm rounded-md focus:outline-none focus:border-blue-400 
+            block w-20 p-1"
+              onChange={(e) => {
+                handleNumberChange(e.target.value);
+              }}
+              input={{
+                type: "number",
+                name: "amountCurrency",
+                id: "amountCurrency",
+                value: inputValue,
+              }}
+            />
+          </Flex>
+        </Flex>
+
+        <Button
+          onClick={() => {
+            dispatch(
+              countCurrencyFromInvoice({
+                baseCurrency: baseCurrency.toLowerCase(),
+                counterCurrency: counterCurrency.toLowerCase(),
+                rate: numberValue ? numberValue : 0,
+              })
+            );
+          }}
+          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded  opacity-100 disabled:opacity-50 "
+          disabled={
+            counterCurrency === baseCurrency ||
+            counterCurrency.length !== 3 ||
+            baseCurrency.length !== 3 ||
+            !Boolean(numberValue)
+          }
+        >
+          następny krok
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default CurrencyEntryInvoice;
