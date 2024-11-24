7d66f86fd1c5f085426d1c0ed4e34990d606b2af
MacJ13 <jacdim02@gmail.com>
2024-11-24 19:12:25 +0100

    Create PrintInvoice component

---
+++ src/features/print/PrintInvoice.tsx
@@ -0,0 +1,38 @@
+import { useSelector } from "react-redux";
+import {
+  selectCurrencyRate,
+  selectInvoiceCurrencies,
+} from "../../redux/currency/currencySlice";
+import Flex from "../../components/flex/Flex";
+import Heading from "../../components/heading/Heading";
+import { upperCase } from "../../utils/helpers/upperCase";
+
+const PrintInvoice = () => {
+  const conversionRate = useSelector(selectCurrencyRate);
+  const { baseCurrency, counterCurrency } = useSelector(
+    selectInvoiceCurrencies
+  );
+
+  const baseCode = upperCase(baseCurrency);
+
+  const counterCode = upperCase(counterCurrency);
+
+  if (!conversionRate) return null;
+
+  return (
+    <Flex className="flex flex-col">
+      <Heading type="h2" className=" mb-4 text-xl">
+        Kurs zgodny z zał. fakturą z wymiany kursu walut z{" "}
+        {<strong>{baseCode}</strong>} na {<strong>{counterCode}</strong>}:
+      </Heading>
+      <Flex className="flex text-3xl justify-center ">
+        <div className="pb-2 border-b-2 border-black">
+          {" "}
+          1 {baseCode} = {conversionRate.toFixed(4)} {counterCode}
+        </div>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default PrintInvoice;
