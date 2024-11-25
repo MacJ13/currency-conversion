e586d06384eaf9da86bb75c2b57de6b7b4383208
MacJ13 <jacdim02@gmail.com>
2024-11-25 22:54:14 +0100

    Print currency rate content

--- src/components/pane/SplitPane.tsx
+++ src/components/pane/SplitPane.tsx
@@ -1,12 +1,32 @@
+import { useRef } from "react";
 import { SplitPaneProps } from "../../utils/types/components";
+import { useReactToPrint } from "react-to-print";
+import Flex from "../flex/Flex";
 
 const SplitPane = ({ left, right }: SplitPaneProps) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const reactToPrintFn = useReactToPrint({ contentRef });
+
   return (
     <div className="flex flex-1">
-      <div className="flex-1 bg-[#fafdff] border-r-4 border-dashed border-sky-800 py-5">
+      <div className=" relative flex-1 bg-[#fafdff] border-r-4 border-dashed border-sky-800 py-5">
+        <Flex className="flex justify-center">
+          <button
+            className="absolute top-0 right-0 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded  opacity-100 disabled:opacity-50"
+            onClick={() => {
+              reactToPrintFn();
+            }}
+          >
+            Drukuj
+          </button>
+        </Flex>
+
         {left}
       </div>
-      <div className="flex-1 py-5">{right}</div>
+      <div ref={contentRef} className="flex-1 py-5">
+        {right}
+      </div>
     </div>
   );
 };
