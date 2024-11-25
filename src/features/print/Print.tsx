da769ca9d05def2e715df193ad2eedeedcff1903
MacJ13 <jacdim02@gmail.com>
2024-11-25 22:18:59 +0100

    Add PrintInvoicesFields to Print component

--- src/features/print/Print.tsx
+++ src/features/print/Print.tsx
@@ -2,13 +2,15 @@ import { useSelector } from "react-redux";
 import { selectCurrencyOrigin } from "../../redux/currency/currencySlice";
 import PrintNBP from "./PrintNBP";
 import PrintInvoice from "./PrintInvoice";
+import PrintInvoiceFields from "./PrintInvoiceFields";
 
 const Print = () => {
   const origin = useSelector(selectCurrencyOrigin);
 
   return (
-    <div className="font-mono">
+    <div className="font-mono flex flex-col gap-14 px-5">
       {origin === "nbp" ? <PrintNBP /> : <PrintInvoice />}
+      <PrintInvoiceFields />
     </div>
   );
 };
